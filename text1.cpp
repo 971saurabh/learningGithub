83396e07b713f1780b29947ccfcd64c4daca23e5
saurabh <sourabh15072002@gmail.com>
2022-12-27 11:05:59 +0530

    adding new file text1.cpp

---
+++ text1.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout << "HELLO" << endl;
+    return 0;
+}
